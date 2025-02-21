feadc2de4355f04187fc338ac171f338ff068de6
alexodisseus <alexodisseus@gmail.com>
2025-02-21 20:52:51 -0300

    Deve haver a funcionalidade Salvar para que os itens sejam cadastrados no banco de dados.

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -13,6 +13,7 @@ import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.sql.SQLException;
 
 
 public class ProdutosDAO {
@@ -23,9 +24,25 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
+        conn = new conectaDAO().connectDB(); 
+        if (conn != null) {
+            try {
+                String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+                PreparedStatement prep = conn.prepareStatement(sql);
+                prep.setString(1, produto.getNome()); 
+                prep.setInt(2, produto.getValor());   
+                prep.setString(3, produto.getStatus());
+                prep.executeUpdate(); 
+            } catch (SQLException erro) {
+                // Tratamento de erro 
+            } finally {
+                try {
+                    if (conn != null) conn.close();
+                } catch (SQLException e) {
+                    // Tratamento de erro 
+                }
+            }
+        }
         
         
     }
@@ -34,6 +51,8 @@ public class ProdutosDAO {
         
         return listagem;
     }
+
+
     
     
     
