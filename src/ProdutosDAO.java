b150e398949f3773990f2deb60ae1d1fcc50e380
alexodisseus <alexodisseus@gmail.com>
2025-02-22 16:08:59 -0300

    Deve haver uma listagem de dados para que todos os itens cadastrados apareçam na tela de listagem.

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -58,13 +58,47 @@ public class ProdutosDAO {
     
     public ArrayList<ProdutosDTO> listarProdutos(){
         
+        Connection conn = null;
+        PreparedStatement prep = null;
+        ResultSet resultset = null;
+        ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+        
+        try {
+            conn = new conectaDAO().connectDB(); 
+            if (conn != null) {
+
+                String sql = "SELECT * FROM produtos";
+                prep = conn.prepareStatement(sql);
+                resultset = prep.executeQuery(); 
+                
+                while (resultset.next()) {
+                    ProdutosDTO produto = new ProdutosDTO();
+                    produto.setId(resultset.getInt("id"));
+                    produto.setNome(resultset.getString("nome"));
+                    produto.setValor(resultset.getInt("valor"));
+                    produto.setStatus(resultset.getString("status"));
+                    listagem.add(produto); 
+                
+                }
+            
+            }
+        
+        } catch (SQLException erro) {
+            // Tratamento de erro 
+        
+        } finally {
+            try {
+                
+                if (resultset != null) resultset.close();
+                if (prep != null) prep.close();
+                if (conn != null) conn.close();
+            
+            } catch (SQLException e) {
+                // Tratamento de erro 
+            }
+        }
         return listagem;
     }
-
-
     
-    
-    
-        
 }
 
